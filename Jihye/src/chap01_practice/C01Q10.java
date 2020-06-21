068ef3498dc3a1f3bcde05ea749b9d463d79c68f
Jihye <wlgul817@gmail.com>
2020-06-21 10:55:15 +0900

    Q10 change

--- Jihye/src/chap01_practice/C01Q10.java
+++ Jihye/src/chap01_practice/C01Q10.java
@@ -7,13 +7,17 @@ public class C01Q10 {
         Scanner stdIn = new Scanner(System.in);
         int a, b;
         System.out.println("a부터 b까지의 합을 구합니다.");
-        do{
-            System.out.print("a의 값 : ");
-            a = stdIn.nextInt();
+        System.out.print("a의 값 : ");
+        a = stdIn.nextInt();
+        System.out.print("b의 값 : ");
+        b = stdIn.nextInt();
+        
+        while (b <= a){
+            System.out.println("a보다 큰 값을 입력하세요!");
             System.out.print("b의 값 : ");
             b = stdIn.nextInt();
-        } while (b < a);
+        }
 
-        System.out.println("합은 " + (b-a) + " 입니다.");
+        System.out.println("b-a 는 " + (b-a) + " 입니다.");
     }
 }
